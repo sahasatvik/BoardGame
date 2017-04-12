ddf3c3751b005651bf57173191e9cd0492511643
sahasatvik <sahasatvik@gmail.com>
2017-04-12 22:41:00 +0530

    Modified MiniMax.java algorithm and structure

--- src/com/github/sahasatvik/game/MiniMax.java
+++ src/com/github/sahasatvik/game/MiniMax.java
@@ -2,44 +2,56 @@
 package com.github.sahasatvik.game;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 
-public abstract class MiniMax {
+public class MiniMax {
 
 	public class ScoredMove {
-		public int score;
 		public Move move;
+		public int score;
+		public ScoredMove (Move move, int score) {
+			this.move = move;
+			this.score = score;
+		}
+	}
+	
+	public ScoredMove bestScoredMove;
+
+	public Move minimax (Game rootGame, int maxDepth) {
+		bestScoredMove = null;
+		minimax(rootGame, rootGame, maxDepth, 0);
+		return bestScoredMove.move;
 	}
 
-	public ScoredMove minimax (Game rootGame, Game currentGame, int depth) {
+	public int minimax (Game rootGame, Game currentGame, int maxDepth, int currentDepth) {
 		List<Move> nextMoves = currentGame.getValidMoves();
-
-		ScoredMove bestScoredMove = new ScoredMove();
-		bestScoredMove.score = (rootGame.getCurrentPlayer() == currentGame.getCurrentPlayer())
-						? Integer.MIN_VALUE : Integer.MAX_VALUE;
+		currentDepth++;
+		
+		if (currentDepth == maxDepth || nextMoves.isEmpty() || currentGame.isOver())
+			return evaluate(currentGame, currentDepth);
+		
+		BinaryOperator<ScoredMove> compareScoredMoves = (currentGame.getCurrentPlayer() == rootGame.getCurrentPlayer())
+								? ((a, b) -> (a.score > b.score ? a : b))
+								: ((a, b) -> (a.score < b.score ? a : b)); 
+		ScoredMove currentScoredMove;
 		int currentScore;
 
-		if (depth == 0 || nextMoves.size() == 0) {
-			bestScoredMove.score = evaluate(currentGame);
-		} else {
-			for (Move move : nextMoves) {
-				currentScore = minimax(rootGame, move.getNewGame(), depth - 1).score - depth;
-				if (rootGame.getCurrentPlayer() == currentGame.getCurrentPlayer()) {
-					if (currentScore > bestScoredMove.score) {
-						bestScoredMove.score = currentScore;
-						bestScoredMove.move = move;
-					}
-				} else {
-					if (currentScore < bestScoredMove.score) {
-						bestScoredMove.score = currentScore;
-						bestScoredMove.move = move;
-					}
-				}
-			}
+		for (Move move : nextMoves) {
+			currentScore = minimax(rootGame, move.getNewGame(), maxDepth, currentDepth);
+			currentScoredMove = new ScoredMove(move, currentScore);
+			bestScoredMove = compareScoredMoves.apply(currentScoredMove, bestScoredMove);
 		}
 
-		return bestScoredMove;
+		return bestScoredMove.score;
 	}
 
-	public abstract int evaluate (Game game);
+	public int evaluate (Game game, int depth) {
+		if (game.hasWon(game.getCurrentPlayer()))
+			return 10 - depth;
+		for (Player p : game.getPlayers())
+			if (p != game.getCurrentPlayer() && game.hasWon(p))
+				return depth - 10;
+		return 0;
+	}
 }
