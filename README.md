8f318fad90eb2285c7b5c0ee205cdfd108f91ac8
Satvik Saha <sahasatvik@gmail.com>
2017-04-25 16:17:59 +0530

    Documented com.github.sahasatvik.game.Game, fixed the buildDocs script.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BoardGame
 A framework for writing Board Games in Java.
+
+## [Documentation](http://htmlpreview.github.io/?http://github.com/sahasatvik/BoardGame/master/docs/index.html)
+The `docs/` folder in this repository contains an extensive documentation of all of the features of every class in the library. You can start at `docs/index.html`.
--- src/com/github/sahasatvik/game/Game.java
+++ src/com/github/sahasatvik/game/Game.java
@@ -4,11 +4,11 @@ package com.github.sahasatvik.game;
 import java.util.List;
 
 /**
- * Game is an interface which defines methods a Game must implement in order to
- * be able to use the <code>com.github.sahasatvik.game</code> framework.
+ * Game is an interface which defines methods a class defining a game must implement
+ * in order to use the {@link com.github.sahasatvik.game} framework.
  *
  * 	@author		Satvik Saha
- * 	@param	<T>	type of Game object implemented
+ * 	@param	<T>	the type of Game object implemented
  * 	@version	0.1.0, 11/04/2017
  * 	@since		0.1.0
  */
@@ -16,18 +16,79 @@ import java.util.List;
 public interface Game<T extends Game> {
 
 	/**
-	 * Gets a List of Players participating in the Game.
+	 * Returns a List of Players participating in the Game.
 	 *
-	 * 	@return			List of Player objects
+	 * 	@return			the List of Player objects
 	 * 	@see	com.github.sahasatvik.game.Player
 	 * 	@since	0.1.0
 	 */
 	public List<Player<T>> getPlayers ();
+
+	/**
+	 * Returns the Player who was the last to make a Move.
+	 *
+	 * 	@return 		the last Player to Move
+	 * 	@see	com.github.sahasatvik.game.Player
+	 * 	@since	0.1.0
+	 */
 	public Player<T> getCurrentPlayer();
+
+	/**
+	 * Returns the Player who will be the next to make a Move.
+	 *
+	 * 	@return			the next Player to Move
+	 * 	@see	com.github.sahasatvik.game.Player
+	 * 	@since	0.1.0
+	 */
 	public Player<T> getNextPlayer ();
+
+	/**
+	 * Returns <code>true</code> if the current Game has been won by a Player.
+	 *
+	 * 	@param	p		the Player to be checked
+	 * 	@return			<code>true</code> if the current Game has been
+	 * 				won by the Player supplied
+	 * 	@see	com.github.sahasatvik.game.Player
+	 * 	@since	0.1.0
+	 */
 	public boolean hasWon (Player<T> p);
+
+	/**
+	 * Returns <code>true</code> if the current Game in over.
+	 * This includes when a Player has won, the Game is drawn or there are no
+	 * possible valid moves.
+	 *
+	 * 	@return			<code>true</code> if the current Game is over
+	 * 	@since	0.1.0
+	 */
 	public boolean isOver ();
+
+	/**
+	 * Applies the given Move on the current Game.
+	 *
+	 * 	@param	m		the Move to be applied on the current Game
+	 * 	@return			<code>true</code> if the Move was made successfully
+	 * 	@see	com.github.sahasatvik.game.Move
+	 * 	@since	0.1.0
+	 */
 	public boolean makeMove (Move<T> m);
+
+	/**
+	 * Returns a List of all possible, valid Moves that can be applied on the
+	 * current Game.
+	 *
+	 * 	@return			the List of all valid Moves
+	 * 	@see	com.github.sahasatvik.game.Move
+	 * 	@since	0.1.0
+	 */
 	public List<Move<T>> getValidMoves ();
+
+	/**
+	 * Returns a copy of the current Game object.
+	 * This copy can be changed without affecting the parent Game object.
+	 * 
+	 * 	@return			the new copy of the current  Game object
+	 * 	@since	0.1.0
+	 */
 	public T getCopy ();
 }
